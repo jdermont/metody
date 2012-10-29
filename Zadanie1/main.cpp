7a5a906f6ed145225e52c0da2b0f2192883882ef
Jacek <ashirenx@gmail.com>
2012-10-29 14:57:37 +0100

    lubie koty i kucyki

--- Zadanie1/main.cpp
+++ Zadanie1/main.cpp
@@ -1,35 +1,55 @@
 // g++ -o main main.cpp tablica.cpp -Wall
 
 #include <iostream>
+#include <string>
 #include "tablica.h"
 #define KMAX 5 // z gory okreslone
 #define ROZ 4*k
 
 using namespace std;
 
+
+
 int main()
 {
     // podawanie danych
-    int k = 0;
+    int k = 1;
+    cout << "Podaj k z zakresu 1.." << KMAX << endl;
     do {
-        if (k > KMAX) cout << "k wieksze niz KMAX. Podaj mniejsze k!" << endl;
+        if (k > KMAX || k < 1) cout << "k niepoprawne!" << endl;
         cout << "Podaj k: "; cin >> k;
-    } while (k > KMAX);
+    } while (k > KMAX || k < 1);
 
     int n = 3*k;
     double x[ROZ]; // x i A maja rozmiar 4*k (a nie n) poniewaz
     double A[ROZ]; // co trzeci element sie powtarza
     double B[k];
-    int t;
-
-    t = 0;
+    int t = 0;
+    double tmp_x;
+    bool tmp;
+    cout << "Podaj rozne wezly x." << endl;
     for (int i=0;i<n;i++) {
-        cout << "Podaj x" << i+1 << ": "; cin >> x[i+t];
+        tmp = false;
+        cout << "Podaj x" << i+1 << ": "; cin >> tmp_x;
+        for (int j=0; j<(i+t); j++) {
+            if (tmp_x == x[j]) {
+                cout << "Podaj inny x." << endl;
+                tmp = true;
+                break;
+            }
+        }
+
+        if (tmp == true) {
+            i--;
+            continue;
+        }
+
+        x[i+t] = tmp_x;
         if ((i+1)%3 == 0) {
             t++;
             x[i+t] = x[i+t-1];
         }
-    }
+	}
 
     t = 0;
     for (int i=0;i<n;i++) {
@@ -53,6 +73,7 @@ int main()
     cout.precision(3);
 
     for (int i= 0;i<ROZ;i++) {
+        cout << "x" << i+1 << " ";
         for (int j=0;j<ROZ;j++) {
             cout << tablica[i][j] << " ";
         }
